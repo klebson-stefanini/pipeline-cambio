447b1b1b3069c9b0045cf4a8b61dc58fc5dd4137
Klebson Stefanini <klebsonstefanini@gmail.com>
2026-07-27 22:40:07 -0300

    preencha src e trata dados

---
+++ src/consulta_ibge.py
@@ -0,0 +1,14 @@
+import requests
+url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+resposta = requests.get(url, timeout=10)
+print("Status:", resposta.status_code) # passo 1: rodar e ver 200
+
+estados = resposta.json() # passo 2: converter
+print("Tipo:", type(estados)) # <class 'list'>
+print("Quantos estados?", len(estados))
+print(estados[0])
+
+print("\nEstados do Nordeste:") # passo 4: navegar e filtrar
+for estado in estados:
+    if estado["regiao"]["sigla"] == "NE":
+        print(f' {estado["sigla"]} - {estado["nome"]}')
